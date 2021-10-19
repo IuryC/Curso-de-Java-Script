dcbc71286ae412c07842a4a854ddad810892d2af
IuryCc <iurycauan0352@hotmail.com>
2021-10-19 15:50:27 -0300

    Iniciando

---
+++ 3_particularidades/js/scripts.js
@@ -0,0 +1,17 @@
+console.log("Primeiro")
+console.log("Segundo")
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// comentário de linha única
+
+/*
+comentário
+de
+várias
+linhas
+*/
---
+++ 4_variaveis/js/scripts.js
@@ -0,0 +1,22 @@
+var teste = 1;
+
+nome = 'Iury';
+var _nome = 'José';
+var $nome = 'Maria';
+var meuPrimeiroNome = 'Cauan';
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(teste);
+console.log(meuPrimeiroNome);
+console.log(nome);
+console.log(_nome);
+console.log($nome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
