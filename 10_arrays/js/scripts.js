7f78c9fea623f7564d25216c4b59dbd3b61ddb1d
IuryC <iurycauan0352@hotmail.com>
2021-12-20 19:33:37 -0300

    20/12/2021

---
+++ 10_arrays/js/scripts.js
@@ -0,0 +1,14 @@
+var arr = [5, "Iury", true, {teste: 1, teste : 2}] //obj -> {}
+console.log(arr)
+
+var arr2 = [1, 2 , 3];
+console.log(arr2)
+
+console.log(arr[1])
+console.log(arr2[1])
+
+arr2[3] = 5;
+
+console.log(arr2[3])
+arr[0] = "Teste";
+console.log(arr[0])
---
+++ 9_objetos/js/scripts.js
@@ -0,0 +1,15 @@
+var obj = {
+    nome: "Iury",
+    idade: 29,
+    profissao: "estudante",
+};
+
+console.log(obj);
+console.log(typeof obj);
+console.log("Nome: " + obj.nome);
+console.log("Idade: " + obj.idade);
+console.log("Profissão: " + obj.profissao);
+
+obj.nome = "Cauan"
+
+console.log(obj.nome)
